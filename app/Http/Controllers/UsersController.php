c897e0954ebf6706017d0701acdcfb16df19a7ac
ruanwenwu <379879523@qq.com>
2019-09-25 23:58:42 +0000

    user info

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     //注册
     public function create(){
         return view('users/create');
+    }
+
+    /**
+     * 显示用户信息
+     */
+    public function show(User $user){
+        return view('users.show',compact('user'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', 'StaticPagesController@home')->name("home");   //首页
 Route::get('/about', 'StaticPagesController@about')->name("about");   //关于
 Route::get('/help', 'StaticPagesController@help')->name("help");   //帮助
 Route::get('/signup','UsersController@create')->name("signup"); //注册
+Route::resource("users",'UsersController');  //用户资源
